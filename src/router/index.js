23b1bd9d791f83569972426a3dd5e792a9ad4a9a
gamsung0388 <ryan9320@gmail.com>
2026-03-30 23:50:25 +0900

    router update

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory  } from 'vue-router'
 import Home from '../views/Home.vue'
 import SutraView from '../views/SutraView.vue'
 
-const routes = [
-  { path: '/', component: Home },
-  { path: '/sutra', component: SutraView }
-]
-
 const router = createRouter({
-  history: createWebHistory(),
-  routes
+  history: createWebHashHistory(), // ⭐ 이걸로 바꿔
+  routes: [
+    { path: '/', component: Home },
+    { path: '/sutra', component: SutraView }
+  ]
 })
 
 export default router
